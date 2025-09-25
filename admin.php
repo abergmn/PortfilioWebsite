da859b3a36a8eb94a8491d06e35fa2a697c97aec
abergmn <sorcery.ramrod.9d@icloud.com>
2025-09-25 16:49:46 +0000

    Updated Project

---
+++ admin.php
@@ -0,0 +1,25 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "portfolio_db");
+
+$result = $conn->query("SELECT * FROM messages");
+
+echo "<h2>Messages Received</h2>";
+echo "<table border='1'>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Message</th>
+        </tr>";
+
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>
+            <td>" . $row['name'] . "</td>
+            <td>" . $row['email'] . "</td>
+            <td>" . $row['message'] . "</td>
+          </tr>";
+}
+
+echo "</table>";
+
+$conn->close();
+?>
